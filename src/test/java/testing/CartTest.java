a7f776f7134206b3d8997527cf151369f249f54d
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-03-20 15:17:14 +0100

    2.22 Disabled and DisplayName annotations

--- src/test/java/testing/CartTest.java
+++ src/test/java/testing/CartTest.java
@@ -1,13 +1,17 @@
 package testing;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Visible name of test class")
 class CartTest {
 
+    @Disabled
     @Test
     void simulateLargeOrder() {
 
