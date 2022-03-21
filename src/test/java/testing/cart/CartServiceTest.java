44d7c74ad299200222cbb7152a8ef08aa2b31c3d
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-03-21 07:31:35 +0100

    5.41 ArgumentCaptor

--- src/test/java/testing/cart/CartServiceTest.java
+++ src/test/java/testing/cart/CartServiceTest.java
@@ -1,6 +1,7 @@
 package testing.cart;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import testing.order.Order;
 import testing.order.OrderStatus;
@@ -157,6 +158,36 @@ class CartServiceTest {
         //when
         //then
         assertThrows(IllegalStateException.class, () -> cartService.processCart(cart));
+
+    }
+
+    @Test
+    void processCartShouldSendToPrepareWithArgumentCapture() {
+
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+
+        ArgumentCaptor<Cart> argumentCaptor = ArgumentCaptor.forClass(Cart.class);
+
+        given(cartHandler.canHandleCart(cart)).willReturn(true);
+
+        //when
+        Cart resultCart = cartService.processCart(cart);
+
+        //then
+        verify(cartHandler).sendToPrepare(argumentCaptor.capture());
+        then(cartHandler).should().sendToPrepare(argumentCaptor.capture());
+
+        assertThat(argumentCaptor.getValue().getOrders().size(), equalTo(1));
+        assertThat(argumentCaptor.getAllValues().size(), equalTo(1));
+
+        assertThat(resultCart.getOrders(), hasSize(1));
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.PREPARING));
 
     }
 }
